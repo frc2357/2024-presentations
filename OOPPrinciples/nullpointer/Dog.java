95cfffeab2c4f035d570b779891ddf2e0ce22e74
NolanCampbell2357 <nolanc1223@gmail.com>
2024-11-13 21:01:23 -0600

    null pointer example

---
+++ OOPPrinciples/nullpointer/Dog.java
@@ -0,0 +1,7 @@
+package nullpointer;
+
+public class Dog {
+    public void bark() {
+        System.out.println("Bark");
+    }
+}
---
+++ OOPPrinciples/nullpointer/NullPointer.java
@@ -0,0 +1,10 @@
+package nullpointer;
+
+public class NullPointer {
+
+  public static Dog fido;
+
+  public static void main(String[] args) {
+    fido.bark();
+  }
+}
